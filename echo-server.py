283673e984e1567fe3c3f2e3fe1029394f511a04
Durbaca Dian-Elena <141216734+Durbace@users.noreply.github.com>
2024-07-24 11:02:00 +0300

    Update echo-server.py

--- echo-server.py
+++ echo-server.py
@@ -6,19 +6,14 @@ def echo_server(host, port):
         s.listen()
         print(f"Serverul ascultă la {host}:{port}")
 
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print(f"Conectat de la {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    conn.sendall(data)
-
+        conn, addr = s.accept()
+        with conn:
+            print(f"Conectat de la {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
 
 if __name__ == "__main__":
-    HOST = '0.0.0.0'
-    PORT = 65432
-
-    echo_server(HOST, PORT)
+    echo_server('0.0.0.0', 65432)
