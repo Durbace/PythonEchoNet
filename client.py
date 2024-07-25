9c4922a8ba8b06dd0bf99f61f1f2735b5c8a5ff4
Durbaca Dian-Elena <141216734+Durbace@users.noreply.github.com>
2024-07-25 22:45:38 +0300

    Update client.py

--- client.py
+++ client.py
@@ -17,21 +17,6 @@ def echo_client(server_host, server_port):
         print("Connection timed out. Server may not be available.")
     except socket.gaierror:
         print(f"Failed to connect to {server_host}. Error with server address or network.")
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(10)
-                s.connect(('localhost', server_port))
-                print(f"Connected to server localhost:{server_port}")
-
-                message = 'Hello, this is an echo test!'
-                s.sendall(message.encode())
-
-                response = s.recv(1024)
-                print(f"Response received from server: {response.decode()}")
-        except socket.timeout:
-            print("Connection to localhost timed out.")
-        except Exception as e:
-            print(f"Failed to connect to localhost as well: {e}")
 
 if __name__ == "__main__":
     server_host = os.getenv('SERVER_HOST', 'echonet-server-service.default.svc.cluster.local')
