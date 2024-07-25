7ec636844d91e8dab679c4e2131d5a4a1b7f1c46
Durbaca Dian-Elena <141216734+Durbace@users.noreply.github.com>
2024-07-25 15:45:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,22 +2,20 @@ import os
 import socket
 
 def echo_client(server_host, server_port):
-    running_in_docker = os.getenv('RUNNING_IN_DOCKER') == 'true'
-
-    if not running_in_docker:
-        server_host = '127.0.0.1' 
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((server_host, server_port))
-        print(f"Conectat la serverul {server_host}:{server_port}")
+        print(f"Connected to server {server_host}:{server_port}")
 
-        message = 'Salut, acesta este un test de echo!'
+        message = 'Hello, this is an echo test!'
         s.sendall(message.encode())
 
         response = s.recv(1024)
-        print(f"Raspuns primit de la server: {response.decode()}")
+        print(f"Response received from server: {response.decode()}")
 
 if __name__ == "__main__":
-    echo_client('host.docker.internal' if os.getenv('RUNNING_IN_DOCKER') == 'true' else '127.0.0.1', 65432)
+    server_host = os.getenv('SERVER_HOST', 'echonet-server-service')
+    server_port = int(os.getenv('SERVER_PORT', 65432))
+    echo_client(server_host, server_port)
+
 
 
