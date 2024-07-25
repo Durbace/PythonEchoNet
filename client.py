b8a9eedb35c7d7f2173833e9f42b2042fdd9f646
Durbaca Dian-Elena <141216734+Durbace@users.noreply.github.com>
2024-07-25 20:25:28 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import socket
 def echo_client(server_host, server_port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(10)  # Set timeout to 10 seconds
+            s.settimeout(10)
             s.connect((server_host, server_port))
             print(f"Connected to server {server_host}:{server_port}")
 
@@ -34,6 +34,6 @@ def echo_client(server_host, server_port):
             print(f"Failed to connect to localhost as well: {e}")
 
 if __name__ == "__main__":
-    server_host = os.getenv('SERVER_HOST', 'echonet-server-service')
+    server_host = os.getenv('SERVER_HOST', 'echonet-server-service.default.svc.cluster.local')
     server_port = int(os.getenv('SERVER_PORT', 65432))
     echo_client(server_host, server_port)
